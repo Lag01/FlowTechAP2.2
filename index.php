68ff8f671b210377426365d35d13ad4b1875a0a1
adammarzuk <adam.marzuk@lycee-cci-gard.fr>
2024-01-30 08:26:37 +0100

    Modifs redirection des liens / images avec .

--- index.php
+++ index.php
@@ -135,7 +135,7 @@
 	</section>
 
 	<!-- FOOTER -->
-	<!-- <?php include 'pages/components/footer.php'; ?> -->
+	<?php include './pages/components/footer.php'; ?>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 	<script>
 		window.onload = function () {
