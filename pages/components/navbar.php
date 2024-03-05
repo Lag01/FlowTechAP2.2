fd16fd61c84c92691d0af1c43a4980e8e35d417d
Azmog <80716388+AzmogEx@users.noreply.github.com>
2024-03-05 11:41:06 +0100

    modif nav

--- pages/components/navbar.php
+++ pages/components/navbar.php
@@ -10,7 +10,7 @@
 <!--FIN CALENDAR GOOGLE-->
 <nav class="navbar navbar-expand-lg bg-flowtech">
 	<div class="container-fluid">
-		<a class="navbar-brand magz text-dark" href="../index.php">FlowTech</a>
+		<a class="navbar-brand magz text-dark" href="/index.php">FlowTech</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
