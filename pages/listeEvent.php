36b9455eb1b9e4793b06095157dd98fd816e1296
Lucas Spitzer <lucas.spitzer@outlook.fr>
2024-05-14 13:24:12 +0200

    test

--- pages/listeEvent.php
+++ pages/listeEvent.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 require_once ('Event.class.php');
 session_start();
 //var_dump($_SESSION['tableauEvent']);
@@ -41,3 +41,62 @@ echo "    }";
 echo "  });";
 echo "}";
 echo "</script>";
+?> -->
+
+<?php
+require_once ('Event.class.php');
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Liste des événements</title>
+</head>
+
+<body>
+    <table border=1>
+        <?php
+        $index = 0;
+        foreach ($_SESSION['tableauEvent'] as $f) {
+            echo "<tr>";
+            echo "<td><input type='text' name='nomEvenement' value='" . $f->getNomEvenement() . "'</td>";
+            echo "<td><input type='text' name='dateEvenement' value='" . $f->getDateEvenement() . "'</td>";
+            echo "<td><button name='modifier' value='" . $index . "'>Modifier</button></td>";
+            echo "<td><button name='supprimer' value='" . $index . "'>Supprimer</button></td>";
+            echo "</tr>";
+            $index++;
+        }
+        ?>
+    </table>
+
+    <script>
+        let buttons = document.getElementsByName('modifier');
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener('click', function () {
+                let index = this.value;
+                let nomEvenement = prompt('Nouveau nom de l\'événement :', <?php echo json_encode($_SESSION['tableauEvent'][$index]->getNomEvenement()); ?>);
+                let dateEvenement = prompt('Nouvelle date de l\'événement :', <?php echo json_encode($_SESSION['tableauEvent'][$index]->getDateEvenement()); ?>);
+                <?php echo '$_SESSION["tableauEvent"][' . $index . ']->setNomEvenement('; ?>nomEvenement<?php echo ');'; ?>
+                <?php echo '$_SESSION["tableauEvent"][' . $index . ']->setDateEvenement('; ?>dateEvenement<?php echo ');'; ?>
+                // Enregistrer les modifications dans la base de données
+                // ...";
+            });
+        }
+
+        let deleteButtons = document.getElementsByName('supprimer');
+        for (let i = 0; i < deleteButtons.length; i++) {
+            deleteButtons[i].addEventListener('click', function () {
+                let index = this.value;
+                if (confirm('Êtes-vous sûr de vouloir supprimer cet événement ?')) {
+                    <?php echo 'unset($_SESSION["tableauEvent"][' . $index . ']);'; ?>
+                    // Supprimer l'événement de la base de données
+                    // ...";
+                }
+            });
+        }
+    </script>
+</body>
+
+</html>
